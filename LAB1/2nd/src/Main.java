3dea4d7c1be3d5339f8d0f1b7e5455e4473657a8
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-03 13:55:17 +0700

    Update

--- LAB1/2nd/src/Main.java
+++ LAB1/2nd/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static int factorial(int n){
+        if (n == 0)
+            return 1;
+        return n * factorial(n-1);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+
+        System.out.printf("Answers is: %d", factorial(n));
+
     }
 }
