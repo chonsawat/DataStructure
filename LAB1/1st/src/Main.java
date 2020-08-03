c9d946543408c4b1bbff218519e0450e5bde5f82
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-03 13:59:40 +0700

    update

---
+++ LAB1/1st/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        double base, height;
+        System.out.print("Enter base: ");
+        base = scanner.nextDouble();
+
+        System.out.print("Enter heght: ");
+        height = scanner.nextDouble();
+
+        double result = 0.5 * base * height;
+        System.out.println("Answer: " + result);
+    }
+}
