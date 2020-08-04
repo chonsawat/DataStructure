eca492fa810b754233fb7c1827694b2af31af68f
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-04 21:37:05 +0700

    Correct height on 1st in LAB1

--- LAB1/1st/src/Main.java
+++ LAB1/1st/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+	// write your code here
         Scanner scanner = new Scanner(System.in);
 
         double base, height;
         System.out.print("Enter base: ");
         base = scanner.nextDouble();
 
-        System.out.print("Enter heght: ");
+        System.out.print("Enter height: ");
         height = scanner.nextDouble();
 
         double result = 0.5 * base * height;
