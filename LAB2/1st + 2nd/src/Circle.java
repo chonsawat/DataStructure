e4070029347b1d1a314b1dd5ad280a21dae83115
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-10 13:27:31 +0700

    (LAB2) Adding comment and rezip file

--- LAB2/1st + 2nd/src/Circle.java
+++ LAB2/1st + 2nd/src/Circle.java
@@ -1,11 +1,14 @@
 public class Circle {
 
+    // Create Attribute
+    public double radius;
+
+    // Constructor
     public Circle(double radius) {
         this.radius = radius;
     }
 
-    public double radius;
-
+    // Method Calculate
     public double area(){
         return Math.PI * Math.pow(this.radius, 2);
     }
@@ -18,14 +21,17 @@ public class Circle {
         return 4.0/3 * Math.PI * Math.pow(radius,3);
     }
 
+    // Method get
     public double getRadius(){
         return radius;
     }
 
+    // Method set
     public void setRadius(double radius){
         this.radius = radius;
     }
 
+    // Method to easy calling show all values in testCircle
     public void show(){
         System.out.printf("Area: %f\n", area());
         System.out.printf("Circumference: %f\n", circumference());
--- LAB2/1st + 2nd/src/testCircle.java
+++ LAB2/1st + 2nd/src/testCircle.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 public class testCircle {
 
     public static void main(String[] args) {
+        // Create Variable
         double radius;
 
+        // Prepare Scanner
         Scanner scanner = new Scanner(System.in);
 
+        // Receive the radius
         System.out.print("Enter radius: ");
         radius = scanner.nextDouble();
 
+        // Create Circle's object
         Circle circle = new Circle(radius);
 
+        // Show all values
         circle.show();
 
+        // Receive new radius
         System.out.print("\nEnter new radius: ");
         circle.setRadius(scanner.nextDouble());
 
+        // Show all values
         circle.show();
 
     }
