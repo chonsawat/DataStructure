64670843fd547efa2c572f7ab9e7f1d14f473e97
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-10 13:06:46 +0700

    Make easy output and Zip it again (LAB2)

--- LAB2/1st + 2nd/src/Circle.java
+++ LAB2/1st + 2nd/src/Circle.java
@@ -7,15 +7,15 @@ public class Circle {
     public double radius;
 
     public double area(){
-        return 3.14 * Math.pow(this.radius, 2);
+        return Math.PI * Math.pow(this.radius, 2);
     }
 
     public double circumference(){
-        return 2 * 3.14 * radius;
+        return 2 * Math.PI * radius;
     }
 
     public double volume(){
-        return 4.0/3 * 3.14 * Math.pow(radius,3);
+        return 4.0/3 * Math.PI * Math.pow(radius,3);
     }
 
     public double getRadius(){
@@ -24,6 +24,13 @@ public class Circle {
 
     public void setRadius(double radius){
         this.radius = radius;
+    }
+
+    public void show(){
+        System.out.printf("Area: %f\n", area());
+        System.out.printf("Circumference: %f\n", circumference());
+        System.out.printf("Volume: %f\n", volume());
+        System.out.println("Radius: " + getRadius());
     }
 
 }
--- LAB2/1st + 2nd/src/testCircle.java
+++ LAB2/1st + 2nd/src/testCircle.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class testCircle {
+
     public static void main(String[] args) {
         double radius;
 
@@ -11,18 +12,12 @@ public class testCircle {
 
         Circle circle = new Circle(radius);
 
-        System.out.printf("Area: %f\n", circle.area());
-        System.out.printf("Circumference: %f\n", circle.circumference());
-        System.out.printf("Volume: %f\n", circle.volume());
-        System.out.println("Radius: " + circle.getRadius());
+        circle.show();
 
         System.out.print("\nEnter new radius: ");
         circle.setRadius(scanner.nextDouble());
 
-        System.out.printf("Area: %f\n", circle.area());
-        System.out.printf("Circumference: %f\n", circle.circumference());
-        System.out.printf("Volume: %f\n", circle.volume());
-        System.out.println("Radius: " + circle.getRadius());
+        circle.show();
 
     }
 }
