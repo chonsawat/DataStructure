e0c6214785b07410329439a170afc2fcee28d53a
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-17 13:09:08 +0700

    (LAB3) Create basic option

---
+++ LAB3/1st/src/testStack.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class testStack {
+
+    public static void main(String[] args) {
+        // Variable
+        int option;
+
+        // Ask for option
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Main menu\n1. Create Stack\n2. Push\n3. Pop\n4. Top\n5. Show\n6. Exit\nPlease choose your choice: ");
+        option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                System.out.println("Exited the program");
+                break;
+        }
+
+    }
+}
