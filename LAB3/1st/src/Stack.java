7ec658e6a0774e7a388222bce996172b1b0100f7
chonsawat <chonsawat.nakanam@kkumail.com>
2020-08-17 13:49:11 +0700

    (LAB3) Stack switch case

--- LAB3/1st/src/Stack.java
+++ LAB3/1st/src/Stack.java
@@ -1,3 +1,46 @@
 public class Stack {
-    
+    int top = -1;
+    double[] stack;
+
+    // Must enter when create stack...
+    public Stack(int size){
+        this.stack = new double[size];
+    }
+
+    void push(double item){
+        // Check stack full
+
+        top++;
+
+        // Push
+    }
+
+    double pop(){
+        // Check empty
+
+        // Pop
+        return stack[top];
+    }
+
+    double top(){
+        return stack[top];
+    }
+
+    boolean empty(){
+        boolean check = top == -1;
+        if (check){
+            System.out.println("Is empty!!");
+            return true;
+        }
+
+        System.out.println("Not empty!!");
+        return false;
+    }
+
+    void show(){
+        System.out.println("Show all values in stack...\nvalues: ");
+        for (int i=top; i>-1; i--){
+            System.out.printf("%f\t", stack[i]);
+        }
+    }
 }
--- LAB3/1st/src/testStack.java
+++ LAB3/1st/src/testStack.java
@@ -5,25 +5,41 @@ public class testStack {
     public static void main(String[] args) {
         // Variable
         int option;
+        double value;
+        Scanner scanner = new Scanner(System.in);
+
+        // Stack must have at least 1
+        Stack stack = new Stack(10);
 
         // Ask for option
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Main menu\n1. Create Stack\n2. Push\n3. Pop\n4. Top\n5. Show\n6. Exit\nPlease choose your choice: ");
         option = scanner.nextInt();
 
         switch (option) {
             case 1:
+                System.out.print("Enter size of stack: ");
+                value = scanner.nextInt();
+                stack = new Stack((int) value);
+                System.out.printf("Created the new stack size: %d !!", (int) value);
                 break;
             case 2:
+                System.out.print("Enter the value do you need to push: ");
+                value = scanner.nextDouble();
                 break;
             case 3:
+                System.out.println("Pop the value : something");
                 break;
             case 4:
+                System.out.println("Now top is value: , index:");
                 break;
             case 5:
+                System.out.println("Showing all values in stack...");
                 break;
             case 6:
                 System.out.println("Exited the program");
+                break;
+            default:
+                System.out.println("Please enter correct integer!!");
                 break;
         }
 
